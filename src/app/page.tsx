20403f5d7710da7888179ef667166178ea09c5ea
AlfanFG <alfanfaturahman10@gmail.com>
2024-11-17 14:13:44 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,61 +11,53 @@ import { Cta } from "@/components/Cta";
 
 import { benefitOne, benefitTwo } from "@/components/data";
 export default function Home() {
-  return (
-    <Container>
-      <Hero />
-      <ScrollAdvanced>
-        <SectionTitle
-          preTitle="Nextly Benefits"
-          title=" Why should you use this landing page"
-        >
-          Nextly is a free landing page & marketing website template for
-          startups and indie projects. Its built with Next.js & TailwindCSS. And
-          its completely open-source.
-        </SectionTitle>
-      </ScrollAdvanced>
-      <ScrollAdvanced>
-        <Benefits data={benefitOne} />
-      </ScrollAdvanced>
-      <ScrollAdvanced>
-        <Benefits imgPos="right" data={benefitTwo} />
-      </ScrollAdvanced>
-      <ScrollAdvanced>
-        <SectionTitle
-          preTitle="Watch a video"
-          title="Learn how to fullfil your needs"
-        >
-          This section is to highlight a promo or demo video of your product.
-          Analysts says a landing page with video has 3% more conversion rate.
-          So, don&apos;t forget to add one. Just like this.
-        </SectionTitle>
-      </ScrollAdvanced>
-      <ScrollAdvanced>
-        <Video videoId="fZ0D0cnR88E" />
-      </ScrollAdvanced>
-      <ScrollAdvanced>
-        <SectionTitle
-          preTitle="Testimonials"
-          title="Here's what our customers said"
-        >
-          Testimonials is a great way to increase the brand trust and awareness.
-          Use this section to highlight your popular customers.
-        </SectionTitle>
-      </ScrollAdvanced>
-      <ScrollAdvanced>
-        <Testimonials />
-      </ScrollAdvanced>
-      <ScrollAdvanced>
-        <div className="flex flex-col">
-          <SectionTitle preTitle="FAQ" title="Frequently Asked Questions">
-            Answer your customers possible questions here, it will increase the
-            conversion rate as well as support or chat requests.
-          </SectionTitle>
-          <Faq />
-        </div>
-      </ScrollAdvanced>
+	return (
+		<Container>
+			<Hero />
 
-      <Cta />
-    </Container>
-  );
+			<SectionTitle
+				preTitle="Nextly Benefits"
+				title=" Why should you use this landing page"
+			>
+				Nextly is a free landing page & marketing website template for startups
+				and indie projects. Its built with Next.js & TailwindCSS. And its
+				completely open-source.
+			</SectionTitle>
+
+			<Benefits data={benefitOne} />
+
+			<Benefits imgPos="right" data={benefitTwo} />
+
+			<SectionTitle
+				preTitle="Watch a video"
+				title="Learn how to fullfil your needs"
+			>
+				This section is to highlight a promo or demo video of your product.
+				Analysts says a landing page with video has 3% more conversion rate. So,
+				don&apos;t forget to add one. Just like this.
+			</SectionTitle>
+
+			<Video videoId="fZ0D0cnR88E" />
+
+			<SectionTitle
+				preTitle="Testimonials"
+				title="Here's what our customers said"
+			>
+				Testimonials is a great way to increase the brand trust and awareness.
+				Use this section to highlight your popular customers.
+			</SectionTitle>
+
+			<Testimonials />
+
+			<div className="flex flex-col">
+				<SectionTitle preTitle="FAQ" title="Frequently Asked Questions">
+					Answer your customers possible questions here, it will increase the
+					conversion rate as well as support or chat requests.
+				</SectionTitle>
+				<Faq />
+			</div>
+
+			<Cta />
+		</Container>
+	);
 }
